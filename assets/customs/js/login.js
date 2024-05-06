23670c858eaf608bcaa320b570e4e1d15525a59a
Sandeep Chawla <sandeepchawla890@gmail.com>
2024-05-06 15:24:50 +0530

    updated

--- assets/customs/js/login.js
+++
@@ -1,4 +0,0 @@
-$("#loginEye").click(function () {
-    $(this).toggleClass('text-primary');
-    ($("#password").attr('type') == 'text') ? $("#password").prop('type', 'password') : $("#password").prop('type', 'text');
-});
